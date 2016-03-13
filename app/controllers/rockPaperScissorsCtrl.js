fb23bf6e6d17a581daa250d97c516bf3a22b2542
Ramesh <ramtata@gmail.com>
2016-03-13 19:28:30 +0000

    Formatting changes

--- app/controllers/rockPaperScissorsCtrl.js
+++ app/controllers/rockPaperScissorsCtrl.js
@@ -1,34 +1,39 @@
-(function () {
-'use strict';
-angular.module("RockPaperScissorsApp")
-.controller('RockPaperScissorsController',  RockPaperScissorsCtrl);
+/**
+** Controller to start the game
+** Starts the game
+** Decides computer choice randomly
+**/
+(function() {
+    'use strict';
+    angular.module("RockPaperScissorsApp")
+        .controller('RockPaperScissorsController', RockPaperScissorsCtrl);
 
-RockPaperScissorsCtrl.$inject = ['$scope' , '$interval', 'rockPaperScissorsService'];
+    RockPaperScissorsCtrl.$inject = ['$scope', '$interval', 'rockPaperScissorsService'];
 
-function RockPaperScissorsCtrl($scope, $interval, rockPaperScissorsService){
+    function RockPaperScissorsCtrl($scope, $interval, rockPaperScissorsService) {
 
-var vm = this;
-vm.humanPlayer = rockPaperScissorsService.getPlayerData();
-vm.computerPlayer = rockPaperScissorsService.getPlayerData();
-vm.choices = rockPaperScissorsService.getGameChoices();
-vm.updateGameResults = rockPaperScissorsService.updateGameResults;
-vm.makeComputerChoice = makeComputerChoice; 
-vm.startGame = startGame;
+        var vm = this;
+        vm.humanPlayer = rockPaperScissorsService.getPlayerData();
+        vm.computerPlayer = rockPaperScissorsService.getPlayerData();
+        vm.choices = rockPaperScissorsService.getGameChoices();
+        vm.updateGameResults = rockPaperScissorsService.updateGameResults;
+        vm.makeComputerChoice = makeComputerChoice;
+        vm.startGame = startGame;
 
 
-  function startGame(index) {
-      if(!isNaN(index) && index <4){
-         vm.humanPlayer.currentSelectedChoice = vm.choices[index] ;
-         makeComputerChoice();
-         vm.matchResult = vm.updateGameResults(vm.humanPlayer, vm.computerPlayer);
-      }
-  }
+        function startGame(index) {
+            if (!isNaN(index) && index < 4) {
+                vm.humanPlayer.currentSelectedChoice = vm.choices[index];
+                makeComputerChoice();
+                vm.matchResult = vm.updateGameResults(vm.humanPlayer, vm.computerPlayer);
+            }
+        }
 
- 
-  function makeComputerChoice(){
-       vm.computerPlayer.currentSelectedChoice = rockPaperScissorsService.getRandomChoice();
-  }
 
-}
+        function makeComputerChoice() {
+            vm.computerPlayer.currentSelectedChoice = rockPaperScissorsService.getRandomChoice();
+        }
+
+    }
 
 }());
